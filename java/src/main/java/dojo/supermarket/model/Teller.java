d7abb5b24e8217d7d217e060df25a8412351782b
Samuel Ytterbrink <say@praqma.net>
2019-03-05 10:51:04 +0100

    Fix issues, to keep me and intellij happy.

--- java/src/main/java/dojo/supermarket/model/Teller.java
+++ java/src/main/java/dojo/supermarket/model/Teller.java
@@ -3,20 +3,20 @@ package dojo.supermarket.model;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Teller {
+class Teller {
 
     private final SupermarketCatalog catalog;
     private List<Offer> offers = new LinkedList<>();
 
-    public Teller(SupermarketCatalog catalog) {
+    Teller(SupermarketCatalog catalog) {
         this.catalog = catalog;
     }
 
-    public void addSpecialOffer(SpecialOfferType offerType, Product product, double argument) {
+    void addSpecialOffer(SpecialOfferType offerType, Product product, double argument) {
         this.offers.add(new Offer(offerType, product, argument));
     }
 
-    public Receipt checksOutArticlesFrom(ShoppingCart theCart) {
+    Receipt checksOutArticlesFrom(ShoppingCart theCart) {
         Receipt receipt = new Receipt();
         List<ProductQuantity> productQuantities = theCart.getItems();
         for (ProductQuantity pq: productQuantities) {
