98b765f2b3f1b4feba46c5183ce94c9e9e362707
Samuel Ytterbrink <say@praqma.net>
2019-03-05 14:10:44 +0100

    Replace if with switch.

--- java/src/main/java/dojo/supermarket/model/Offer.java
+++ java/src/main/java/dojo/supermarket/model/Offer.java
@@ -20,34 +20,34 @@ class Offer {
     NewOffer createNewOffer() {
         return (Product product, double quantity, double unitPrice) -> {
             if (getProduct().equals(product)) {
-                if (offerType == SpecialOfferType.ThreeForTwo) {
-                    if ((int) quantity > 2) {
-                        int numberOfXs = (int) quantity / 3;
-                        double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + (int) quantity % 3 * unitPrice);
-                        return Stream.of(new Discount(product, "3 for 2", discountAmount));
-                    }
-
-                } else if (offerType == SpecialOfferType.TwoForAmount) {
-                    if ((int) quantity >= 2) {
-                        double total = argument * (int) quantity / 2 + (int) quantity % 2 * unitPrice;
-                        double discountN = unitPrice * quantity - total;
-                        return Stream.of(new Discount(product, "2 for " + argument, discountN));
-                    }
-
-                }
-                if (offerType == SpecialOfferType.FiveForAmount) {
-                    int numberOfXs = (int) quantity / 5;
-                    if ((int) quantity >= 5) {
-                        double discountTotal = unitPrice * quantity - (argument * numberOfXs + (int) quantity % 5 * unitPrice);
-                        return Stream.of(new Discount(product, 5 + " for " + argument, discountTotal));
-                    }
-                }
-                if (offerType == SpecialOfferType.TenPercentDiscount) {
-                    return Stream.of(new Discount(
-                        product,
-                        argument + "% off",
-                        quantity * unitPrice * argument / 100.0
-                    ));
+                switch (offerType) {
+                    case ThreeForTwo:
+                        if ((int) quantity > 2) {
+                            int numberOfXs = (int) quantity / 3;
+                            double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + (int) quantity % 3 * unitPrice);
+                            return Stream.of(new Discount(product, "3 for 2", discountAmount));
+                        }
+                        break;
+                    case TwoForAmount:
+                        if ((int) quantity >= 2) {
+                            double total = argument * (int) quantity / 2 + (int) quantity % 2 * unitPrice;
+                            double discountN = unitPrice * quantity - total;
+                            return Stream.of(new Discount(product, "2 for " + argument, discountN));
+                        }
+                        break;
+                    case FiveForAmount:
+                        int numberOfXs = (int) quantity / 5;
+                        if ((int) quantity >= 5) {
+                            double discountTotal = unitPrice * quantity - (argument * numberOfXs + (int) quantity % 5 * unitPrice);
+                            return Stream.of(new Discount(product, 5 + " for " + argument, discountTotal));
+                        }
+                        break;
+                    case TenPercentDiscount:
+                        return Stream.of(new Discount(
+                            product,
+                            argument + "% off",
+                            quantity * unitPrice * argument / 100.0
+                        ));
                 }
             }
             return Stream.empty();
