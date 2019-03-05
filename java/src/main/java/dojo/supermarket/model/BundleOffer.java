0967bcca33f89371e1b436c40e204e890fb54d4b
Samuel Ytterbrink <say@praqma.net>
2019-03-05 15:22:05 +0100

    Implement concat and and for BundleOffer.

--- java/src/main/java/dojo/supermarket/model/BundleOffer.java
+++ java/src/main/java/dojo/supermarket/model/BundleOffer.java
@@ -6,4 +6,21 @@ import java.util.stream.Stream;
 public interface BundleOffer {
 
     Stream<Discount> getDiscounts(Map<Product, Double> productQuantities, SupermarketCatalog catalog);
+
+    default BundleOffer and(BundleOffer other) {
+        return (productQuantities, catalog) -> {
+            if(this.getDiscounts(productQuantities, catalog).findAny().isPresent()) {
+                return this.concat(other).getDiscounts(productQuantities, catalog);
+            } else {
+                return Stream.empty();
+            }
+        };
+    }
+
+    default BundleOffer concat(BundleOffer other) {
+        return (productQuantities, catalog) -> Stream.concat(
+            this.getDiscounts(productQuantities, catalog),
+            other.getDiscounts(productQuantities, catalog)
+        );
+    }
 }
